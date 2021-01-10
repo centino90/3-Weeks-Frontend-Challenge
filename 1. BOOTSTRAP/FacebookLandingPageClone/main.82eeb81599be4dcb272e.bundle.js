040d9f0b43cfc1b0d891a8d3615d96976765a77a
centino90 <anthonyjayansit2326282@gmail.com>
2021-01-10 19:12:02 +0800

    second project

---
+++ 1. BOOTSTRAP/FacebookLandingPageClone/main.82eeb81599be4dcb272e.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const s=new class{constructor(s,e){this.fname=s,this.lname=e}spitAddress(s,e,t,i){return`${n=this.fname,a=this.lname,`${n} ${a}`} lives in ${function(s,e,t,i){return`${s}, ${e}, ${t}, ${i}`}(s,e,t,i)}`;var n,a}}("John","Doe");alert(s.spitAddress("km 11 Bayview","Sasa","Davao","Phillippines"))})();
